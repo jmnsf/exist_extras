9cc74550c2f2ca3b0808f04e893357052101f442
jmnsf <jmnsferreira@gmail.com>
2017-08-01 23:27:20 +1000

    Deploy script fixes and env vars addition
    Signed-off-by: jmnsf <jmnsferreira@gmail.com>

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,15 +2,15 @@ docker-compose run release
 
 scp -i devops/exist-extras.pem \
   _build/prod/rel/exist_extras/releases/0.1.0/exist_extras.tar.gz \
-  ec2-user@exist.jmnsf.com:exist_extras.tar.gz
+  ec2-user@35.166.205.77:exist_extras.tar.gz
 
 scp -i devops/exist-extras.pem \
-  Dockerfile ec2-user@exist.jmnsf.com:Dockerfile
+  Dockerfile ec2-user@35.166.205.77:Dockerfile
 
 scp -i devops/exist-extras.pem \
-  docker-compose.yml ec2-user@exist.jmnsf.com:docker-compose.yml
+  docker-compose.yml ec2-user@35.166.205.77:docker-compose.yml
 
-ssh -i devops/exist-extras.pem ec2-user@exist.jmnsf.com 'tar -xzvf exist_extras.tar.gz'
+ssh -i devops/exist-extras.pem ec2-user@35.166.205.77 'tar -xzvf exist_extras.tar.gz'
 
-ssh -i devops/exist-extras.pem ec2-user@exist.jmnsf.com \
-  'docker-compose down prod && docker-compose up -d prod'
+ssh -i devops/exist-extras.pem ec2-user@35.166.205.77 \
+  'docker-compose down && docker-compose up -d prod'
